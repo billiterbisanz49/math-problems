72bd4f04daa5255a5c44fb33fadaf1620332fd73
billiterbisanz49 <billiterbisanz49@gmail.com>
2025-04-11 12:32:15 +0000

    Add 32.js

---
+++ src/32.js
@@ -0,0 +1,20 @@
+function addNumbers(x, y) {
+  return x + y;
+}
+
+function multiplyNumbers(a, b) {
+  return a * b;
+}
+
+function calculateBMI(weight, height) {
+  const bmi = weight / (height * height);
+  if (bmi < 18.5) {
+    return "Underweight";
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    return "Normal";
+  } else if (bmi > 24.9 && bmi <= 29.9) {
+    return "Overweight";
+  } else {
+    return "Obese";
+  }
+}
