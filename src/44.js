094ab63af2e93f028331f1058fcaedb2d47d124f
billiterbisanz49 <billiterbisanz49@gmail.com>
2025-04-11 12:54:22 +0000

    Add 44.js

---
+++ src/44.js
@@ -0,0 +1,6 @@
+function calculateArea(radius) {
+    let area = Math.PI * radius * radius;
+    console.log(`The area of the circle with radius ${radius} is: ${area.toFixed(2)}`);
+}
+
+calculateArea(5);
