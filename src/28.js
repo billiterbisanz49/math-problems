70db4a3deb22463d3696023d5d1d3e1d931aaefa
billiterbisanz49 <billiterbisanz49@gmail.com>
2025-04-11 11:22:18 +0000

    Add 28.js

---
+++ src/28.js
@@ -0,0 +1,29 @@
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i * i <= num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function sumOfDigits(n) {
+  let sum = 0;
+  while (n > 0) {
+    sum += n % 10;
+    n = Math.floor(n / 10);
+  }
+  return sum;
+}
+
+function calculateGCD(a, b) {
+  while (b !== 0) {
+    let temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+}
