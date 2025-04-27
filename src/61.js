90c1572645214036123e14f369434f6fde93a779
billiterbisanz49 <billiterbisanz49@gmail.com>
2025-04-27 07:54:05 +0000

    Add 61.js

---
+++ src/61.js
@@ -0,0 +1,7 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Example usage
+const randomNumber = getRandomInt(10);
+console.log(`Random number: ${randomNumber}`);
