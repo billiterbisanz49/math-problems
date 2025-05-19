2eeb4b2ae89b41a88063acc8e7cf9182c77f0be0
billiterbisanz49 <billiterbisanz49@gmail.com>
2025-05-19 01:13:24 +0000

    Add 63.js

---
+++ src/63.js
@@ -0,0 +1,11 @@
+function addNumbers(a, b) {
+    return a + b;
+}
+
+console.log(addNumbers(2, 3)); // Output: 5
+
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+console.log(multiplyNumbers(4, 6)); // Output: 24
