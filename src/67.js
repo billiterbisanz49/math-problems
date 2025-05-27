943a2d3a90af8d8839881bc74db2c95b73fd3761
billiterbisanz49 <billiterbisanz49@gmail.com>
2025-05-27 02:53:09 +0000

    Add 67.js

---
+++ src/67.js
@@ -0,0 +1,7 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// Example usage: Generate a random number between 1 and 10
+let randomNumber = getRandomInt(10);
+console.log(randomNumber);
